087acc96381756999f2678b73bf3b10589fc8b1f
danila seliayeu <dan.seliayeu@gmail.com>
2025-07-01 10:39:12 -0300

    remove comments

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Example nix-darwin system flake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
@@ -12,8 +10,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       programs.direnv = {
         enable = true;
         nix-direnv.enable = true;
@@ -60,32 +56,20 @@
         };
       };
 
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.primaryUser = "dan";
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 6;
-
       system.defaults.dock.autohide = true;
+      system.defaults.NSGlobalDomain._HIHideMenuBar = true;
 
-       system.defaults.NSGlobalDomain._HIHideMenuBar = true;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
       security.pam.services.sudo_local.touchIdAuth = true;
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#dan-MacbookAir-6
     darwinConfigurations."dan-MacbookAir-6" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
